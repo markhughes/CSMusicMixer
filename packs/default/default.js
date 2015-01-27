571a0aaa70d86b9596cd22ec6323cf20e18d4ad1
Mark Hughes <mark@markeh.me>
2015-01-27 17:56:53 +1100

    Changing for mp3s to wav files

--- packs/default/default.js
+++ packs/default/default.js
@@ -50,63 +50,63 @@ var pack = "Country";
 Packs.addSound({
 	pack_id: pack,
 	label: "Country 1 Bass",
-	file: "countryOne_bass00.mp3",
+	file: "countryOne_bass00.wav",
 	icon: "packs/default/icons/bass.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Country 1 Guitar",
-	file: "countryOne_guitar00.mp3",
+	file: "countryOne_guitar00.wav",
 	icon: "packs/default/icons/guitar.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Country 2 Banjo",
-	file: "countryTwo_banjo00.mp3",
+	file: "countryTwo_banjo00.wav",
 	icon: "packs/default/icons/banjo.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Country 2 Bass",
-	file: "countryTwo_bass00.mp3",
+	file: "countryTwo_bass00.wav",
 	icon: "packs/default/icons/banjo.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Country 2 Beatz 1",
-	file: "countryTwo_beatz00.mp3",
+	file: "countryTwo_beatz00.wav",
 	icon: "packs/default/icons/banjo.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Country 2 Beatz 2",
-	file: "countryTwo_beatz01.mp3",
+	file: "countryTwo_beatz01.wav",
 	icon: "packs/default/icons/banjo.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Strings",
-	file: "danceTwo_strings00.mp3",
+	file: "danceTwo_strings00.wav",
 	icon: "packs/default/icons/strings.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Cowbell",
-	file: "misc_cowbell00.mp3",
+	file: "misc_cowbell00.wav",
 	icon: "packs/default/icons/bells.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Harmonica",
-	file: "misc_harmonica00.mp3",
+	file: "misc_harmonica00.wav",
 	icon: "packs/default/icons/harmonica.png"
 });
 
@@ -121,49 +121,49 @@ var pack = "Dance";
 Packs.addSound({
 	pack_id: pack,
 	label: "Dance 1 Bass",
-	file: "danceOne_bass00.mp3",
+	file: "danceOne_bass00.wav",
 	icon: "packs/default/icons/bass.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Dance 1 Beatz 1",
-	file: "danceOne_beatz00.mp3",
+	file: "danceOne_beatz00.wav",
 	icon: "packs/default/icons/bass.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Dance 1 Beatz 2",
-	file: "danceOne_beatz01.mp3",
+	file: "danceOne_beatz01.wav",
 	icon: "packs/default/icons/bass.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Dance 2 Effect",
-	file: "danceTwo_effect00.mp3",
+	file: "danceTwo_effect00.wav",
 	icon: "packs/default/icons/bass.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Dance 2 Strings",
-	file: "danceTwo_strings00.mp3",
+	file: "danceTwo_strings00.wav",
 	icon: "packs/default/icons/bass.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Jazz Guitar",
-	file: "misc_jazzGuit00.mp3",
+	file: "misc_jazzGuit00.wav",
 	icon: "packs/default/icons/guitar.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Shaker",
-	file: "misc_shaker00.mp3",
+	file: "misc_shaker00.wav",
 	icon: "packs/default/icons/shaker.png"
 });
 
@@ -171,21 +171,21 @@ Packs.addSound({
 Packs.addSound({
 	pack_id: pack,
 	label: "Tubular Bells",
-	file: "misc_tubularBells00.mp3",
+	file: "misc_tubularBells00.wav",
 	icon: "packs/default/icons/bells.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Vocals: Higher!",
-	file: "misc_vocal04.mp3",
+	file: "misc_vocal04.wav",
 	icon: "packs/default/icons/vocal.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Pop 2 Bass",
-	file: "popTwo_bass00.mp3",
+	file: "popTwo_bass00.wav",
 	icon: "packs/default/icons/vocal.png"
 });
 
@@ -200,56 +200,56 @@ var pack = "Hip-Hop";
 Packs.addSound({
 	pack_id: pack,
 	label: "Hip-Hop 1 Bass",
-	file: "hipHopOne_bass00.mp3",
+	file: "hipHopOne_bass00.wav",
 	icon: "packs/default/icons/vocal.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Hip-Hop 1 Beatz",
-	file: "hipHopOne_beatz01.mp3",
+	file: "hipHopOne_beatz01.wav",
 	icon: "packs/default/icons/vocal.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Hip-Hop 1 KDB Effect ",
-	file: "hipHopOne_kbd00.mp3",
+	file: "hipHopOne_kbd00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Hip-Hop 2 Beatz ",
-	file: "hipHopTwo_beatz00.mp3",
+	file: "hipHopTwo_beatz00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Buzz Effect ",
-	file: "misc_effect00.mp3",
+	file: "misc_effect00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Fast Scratch ",
-	file: "misc_fastScratch00.mp3",
+	file: "misc_fastScratch00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Vocals: Feel it",
-	file: "misc_vocal02.mp3",
+	file: "misc_vocal02.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Vocals: Bass",
-	file: "misc_vocal03.mp3",
+	file: "misc_vocal03.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
@@ -269,63 +269,63 @@ var pack = "Latin";
 Packs.addSound({
 	pack_id: pack,
 	label: "Sax",
-	file: "latinOne_sax00.mp3",
+	file: "latinOne_sax00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Percussion 1",
-	file: "latinoOne_percCombo00.mp3",
+	file: "latinoOne_percCombo00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Marracas",
-	file: "latinoTwo_marracas00.mp3",
+	file: "latinoTwo_marracas00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Percussion 2",
-	file: "latinoTwo_percCombo00.mp3",
+	file: "latinoTwo_percCombo00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Piano",
-	file: "latinoTwo_piano00.mp3",
+	file: "latinoTwo_piano00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Jazz Trumpet",
-	file: "misc_jazzTrumpet00.mp3",
+	file: "misc_jazzTrumpet00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Rachet",
-	file: "misc_rachet00.mp3",
+	file: "misc_rachet00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Crash",
-	file: "shared_crash00.mp3",
+	file: "shared_crash00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Latino Bells",
-	file: "shared_latinoBells00.mp3",
+	file: "shared_latinoBells00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
@@ -338,63 +338,63 @@ var pack = "Pop";
 Packs.addSound({
 	pack_id: pack,
 	label: "Misc Bass",
-	file: "misc_bass00.mp3",
+	file: "misc_bass00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Funk Guitar",
-	file: "misc_funkGuit00.mp3",
+	file: "misc_funkGuit00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Spanish Guitar",
-	file: "misc_spanishGuit00.mp3",
+	file: "misc_spanishGuit00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Beat Boxing",
-	file: "misc_vocal00.mp3",
+	file: "misc_vocal00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Pop 1 Keyboard",
-	file: "popOne_kbd00.mp3",
+	file: "popOne_kbd00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Vocals: Come on!",
-	file: "popOne_vocal00.mp3",
+	file: "popOne_vocal00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Pop 2 Bass",
-	file: "popTwo_bass00.mp3",
+	file: "popTwo_bass00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Pop 2 Beatz",
-	file: "popTwo_beatz00.mp3",
+	file: "popTwo_beatz00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Pop 2 Keyboard",
-	file: "popTwo_kbd00.mp3",
+	file: "popTwo_kbd00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
@@ -408,63 +408,63 @@ var pack = "Rock";
 Packs.addSound({
 	pack_id: pack,
 	label: "Cowbell",
-	file: "misc_cowbell00.mp3",
+	file: "misc_cowbell00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Eastern Percussion",
-	file: "misc_easternPerc00.mp3",
+	file: "misc_easternPerc00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Rock 1 Bass",
-	file: "rockOne_bass00.mp3",
+	file: "rockOne_bass00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Rock 1 Beatz",
-	file: "rockOne_beatz00.mp3",
+	file: "rockOne_beatz00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Rock 1 Cymbal",
-	file: "rockOne_cymbal00.mp3",
+	file: "rockOne_cymbal00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Rock 1 Guitar 1",
-	file: "rockOne_guitar00.mp3",
+	file: "rockOne_guitar00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Rock 1 Guitar 2",
-	file: "rockOne_guitar01.mp3",
+	file: "rockOne_guitar01.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Rock 2 Beatz",
-	file: "rockTwo_beatz00.mp3",
+	file: "rockTwo_beatz00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
 Packs.addSound({
 	pack_id: pack,
 	label: "Crash",
-	file: "shared_crash00.mp3",
+	file: "shared_crash00.wav",
 	icon: "packs/default/icons/fx.png"
 });
 
