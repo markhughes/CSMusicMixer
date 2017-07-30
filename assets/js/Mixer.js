4bba2db8f9e2b39ca2674c21f1bd1536815d81c2
Mark Hughes <m@rkhugh.es>
2017-07-31 00:44:40 +1000

    Add share URLs

--- assets/js/Mixer.js
+++ assets/js/Mixer.js
@@ -1,18 +1,18 @@
 /*
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2015 Mark William Hughes
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,9 +20,9 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- * 
+ *
  */
- 
+
 var Mixer = Mixer || {};
 
 Mixer._slots = [];
@@ -31,37 +31,37 @@ Mixer.currentPack = null;
 
 Mixer.removeStored = function (rem) {
 	var workingWith = JSON.parse(localStorage.getItem("savedTracks"));
-	
+
 	workingWith.splice(rem, 1);
-	
-	
+
+
 	localStorage.setItem("savedTracks", JSON.stringify(workingWith));
-	
-	// make sure we rebuild too - it's required! 
+
+	// make sure we rebuild too - it's required!
 	Mixer.loadExistingTracks();
-	
+
 }
 
-Mixer.doLoad = function(collection, trackData, skipURIDecode, silent) {	
+Mixer.doLoad = function(collection, trackData, skipURIDecode, silent) {
 	var start = new Date();
-	
+
 	showStage("mixer");
-	
+
 	Mixer.setPack(collection);
-	
-	
+
+
 	if(skipURIDecode == null) {
 		trackData = (decodeURI(trackData));
 	}
-	
+
 	trackData = JSON.parse(trackData);
-	
+
 	// show the mixer so our elements are there
-	
+
 	Mixer.buildBlankRows();
-	
+
 	var row = 0;
-	
+
 	trackData.forEach(function (data) {
 		if(data.length > 0) {
 			data.forEach(function (sample_id) {
@@ -70,53 +70,53 @@ Mixer.doLoad = function(collection, trackData, skipURIDecode, silent) {
 					return;
 				}
 			}, this);
-			
+
 			Mixer.setRow(row);
-			
+
 			var col = 1;
-			
+
 			data.forEach(function (sample_id) {
 				if(sample_id != "_" && sample_id != "") {
 					Mixer.toggle(row, col);
 				}
-				
+
 				col = col + 1;
 			}, this);
 		}
-		
+
 		row++;
 	});
-	
+
 	if(silent != null) return;
-	
+
 	Interface.showMessage("Loaded!", "The track has been loaded in " + (new Date() - start) + "ms", null, {yes : true});
-	
+
 }
 
 
 Mixer.loadExistingTracks = function() {
 	var built = "<ul>";
-	
+
 	Mixer.currentI = 0;
-	
+
 	if(localStorage.getItem("savedTracks") != null) {
 		var st = JSON.parse(localStorage.getItem("savedTracks"))
-		
+
 		st.forEach(function (a) {
 			var splitUp = a.split("||[\/@]/@||");
-			
+
 			built+= "<li><a href=\"#\" onclick=\"Mixer.doLoad('"+encodeURI(splitUp[1])+"', '"+encodeURI(splitUp[2])+"');\">"+splitUp[0]+"</a> <a href=\"#\" onclick=\"Mixer.removeStored("+JSON.parse(localStorage.getItem("savedTracks")).indexOf(a)+");\" class=\"removeButton\">X</a></li>";
-			
+
 			Mixer.currentI++;
 		});
 	}
-	
+
 	built+= "</ul>";
-	
+
 	if(Mixer.currentI ==0) {
 		built = "You have no stored tracks!";
 	}
-	
+
 	document.getElementById("storedTracksList").innerHTML = built;
 }
 
@@ -124,26 +124,26 @@ Mixer.setSample = function(sample_id, row) {
 	Mixer._slots[row] = sample_id;
 	document.getElementById("row"+row+"_option").value = Packs.soundLabels[sample_id];
 	document.getElementById("row"+row+"_option").setAttribute("sample_id", sample_id);
-	
+
 }
 
 Mixer.setPack = function(pack) {
-	
+
 	document.getElementByClass("Country_class").style.color	= '#000000';
 	document.getElementByClass("Dance_class").style.color	= '#000000';
 	document.getElementByClass("Hip-Hop_class").style.color	= '#000000';
 	document.getElementByClass("Latin_class").style.color	= '#000000';
 	document.getElementByClass("Pop_class").style.color		= '#000000';
 	document.getElementByClass("Rock_class").style.color	= '#000000';
-	
+
 	if(document.getElementByClass(pack+"_class") != null) {
 		document.getElementByClass(pack+"_class").style.color = 'red';
 	}
-	
+
 	Packs.show(pack);
 	Mixer.currentPack = pack;
-	
-	
+
+
 }
 
 Mixer.getSample = function(row) {
@@ -158,9 +158,9 @@ Mixer.getLengthInSeconds = function(row) {
 	var dur = Math.floor(this.getSound(row).durationEstimate/1000);
 	if(dur == 8) dur = 10;
 	if(dur == 7) dur = 10;
-	
+
 	if(dur < 2) dur = 2;
-	
+
 	return dur;
 }
 
@@ -169,23 +169,23 @@ Mixer.getStepsLayout = function(row, size) {
 		var str = "<img id='s_"+row+"_%i%' src='assets/images/tabs/2s_clear.png' onclick='Mixer.toggle("+row+", %i%)' alt='' width='18' height='36' selected='0'>";
 		return(str.repeat(30));
 	}
-	
+
 	if(size == 4) {
 		var str = "<img id='s_"+row+"_%i%' src='assets/images/tabs/2s_clear.png' width='36' height='36' onclick='Mixer.toggle("+row+", %i%)' selected='0'>";
 		return(str.repeat(15));
 	}
-	
+
 	if(size == 10) {
 		var str = "<img id='s_"+row+"_%i%' src='assets/images/tabs/2s_clear.png' width='90' height='36' onclick='Mixer.toggle("+row+", %i%)' selected='0'>"
 		return(str.repeat(6));
 	}
-	
+
 	return null
 }
 
-Mixer.buildBlankRows = function() {	
+Mixer.buildBlankRows = function() {
 	var rows = '';
-	
+
 	var row_template = ''+
 '			<div id="row" class="row%i% two">'+
 '				<div id="leftbox" ondrop="return(Mixer.droppedOn(event))" ondragover="event.preventDefault();" row="%i%">'+
@@ -195,14 +195,14 @@ Mixer.buildBlankRows = function() {
 '				</div>'+
 '				<div id="rightbox" class="rightbox-%i%"></div>'+
 '			</div>';
-	
+
 	var i = 0;
 	while(i < 6) {
 		var working_row = row_template;
 		rows+= working_row.replace(new RegExp("%i%", 'g'), i);
 		i++;
 	}
-	
+
 	document.getElementById("rows-collection").innerHTML = rows;
 }
 
@@ -216,18 +216,18 @@ Mixer.setRow = function(row) {
 
 Mixer.toggle = function(row, tab) {
 	var tab = document.getElementById("s_"+row+"_"+tab);
-	
+
 	if(tab == null) return;
-	
+
 	if(tab.getAttribute("selected") == "0") {
 		tab.setAttribute("selected", "1");
 		tab.setAttribute("src", "assets/images/tabs/2s_clear_enabled.png");
-		
+
 	} else {
 		tab.setAttribute("selected", "0");
 		tab.setAttribute("src", "assets/images/tabs/2s_clear.png");
 	}
-	
+
 	StepSequence.buildSequence();
 }
 
@@ -252,16 +252,16 @@ Mixer.playStopButton = function() {
 Mixer.clearAll = function() {
 	StepSequence.stop();
 	Mixer.buildBlankRows();
-	
+
 	StepSequence._set[0] = [];
 	StepSequence._set[1] = [];
 	StepSequence._set[2] = [];
 	StepSequence._set[3] = [];
 	StepSequence._set[4] = [];
 	StepSequence._set[5] = [];
-	
+
 	StepSequence.buildSequence(); // Make sure we rebuild the sequence, otherwise it's still store in code
-	
+
 	// force stop the song just in case its playing
 	document.getElementById("playButton").setAttribute("src", "assets/images/play.png");
 	document.getElementById("playButton").setAttribute("class", "s_stopped");
@@ -275,14 +275,14 @@ Mixer.save = function() {
 			if(localStorage.getItem("savedTracks") == null) {
 				localStorage.setItem("savedTracks", JSON.stringify([]));
 			}
-			
+
 			var tracks = JSON.parse(localStorage.getItem("savedTracks"));
 			tracks.push(results.value+"||[\/@]/@||"+0+"||[\/@]/@||"+JSON.stringify(StepSequence._set));
-									
+
 			localStorage.setItem("savedTracks", JSON.stringify(tracks));
-			
+
 			Mixer.loadExistingTracks();
-			
+
 			Interface.showMessage("Saved", "Your track has been saved!", null, { yes: true} );
 		}
 	});
@@ -292,7 +292,8 @@ Mixer.share = function() {
 	window.lzmalib.compress(
 		JSON.stringify(0+"||[\/@]/@||"+JSON.stringify(StepSequence._set)), 1,
 		function on_compress_complete(str) {
-			var content = "Here is your share key:<br><textarea name=\"textarea\" rows=\"10\" cols=\"40\">" + str.toString().replaceAll(",", " ") + "</textarea>";
+			var shareable = str.toString().replaceAll(",", " ");
+			var content = "Here is your share key:<br><textarea name=\"textarea\" rows=\"10\" cols=\"40\">" + shareable + "</textarea><br>or, <a href=\"https://is.gd/create.php?url="+encodeURI("https://markehme.github.io/CSMusicMixer/index.html?load="+Base64.encode(shareable))+"\">click here</a> to get a share link.";
 			Interface.showMessage("Share this!", content, null, {yes:true});
 		}
 	);
@@ -300,27 +301,27 @@ Mixer.share = function() {
 
 Mixer.doImport = function(str) {
 	showStage("load");
-	
+
 	loadingStatus("Loading track ...");
-	
+
 	window.lzmalib.decompress(str.split(" "), function on_decompress_complete(str) {
-		var data = str.split("||[/@]/@||");		
+		var data = str.split("||[/@]/@||");
 		showStage("mixer");
 		var filtered_string = data[1].replace(/\\"/g,'"');
-		
-		Mixer.doLoad(data[0].substring(1), filtered_string.substring(0, filtered_string.length-1), true);		
+
+		Mixer.doLoad(data[0].substring(1), filtered_string.substring(0, filtered_string.length-1), true);
 	});
 }
 
 Mixer.importDefaultForPack = function(pack) {
 	if(Packs.defaultTracks[pack] == null) return;
-	
+
 	window.lzmalib.decompress(Packs.defaultTracks[pack].split(" "), function on_decompress_complete(str) {
-		var data = str.split("||[/@]/@||");		
+		var data = str.split("||[/@]/@||");
 		showStage("mixer");
 		var filtered_string = data[1].replace(/\\"/g,'"');
-		
-		Mixer.doLoad(data[0].substring(1), filtered_string.substring(0, filtered_string.length-1), true, true);		
+
+		Mixer.doLoad(data[0].substring(1), filtered_string.substring(0, filtered_string.length-1), true, true);
 	});
 }
 
@@ -331,14 +332,13 @@ Mixer.changeVolume = function(row, value) {
 
 Mixer.droppedOn = function(event) {
 	window.daEvent = event;
-	
+
 	var elementTo = event.relatedTarget || event.toElement || event.target;
 	console.log(elementTo);
-	
+
 	document.getElementById("row"+elementTo.getAttribute("row")+"_option").value = window.dragID;
-	
+
 	Mixer.changeSampleAtRow(elementTo.getAttribute("row"));
-	
+
 	return false;
 }
-
--- assets/js/Packs.js
+++ assets/js/Packs.js
@@ -1,18 +1,18 @@
 /*
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2015 Mark William Hughes
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,9 +20,9 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- * 
+ *
  */
- 
+
 var Packs = Packs || {};
 
 /**
@@ -57,7 +57,7 @@ Packs.defaultTracks = [];
  *  path:		a path to where the sounds are
  *
  */
-Packs.setSoundPath = function(pack, path) { 
+Packs.setSoundPath = function(pack, path) {
 	this.soundsPaths[pack] = path;
 };
 
@@ -65,37 +65,37 @@ Packs.setSoundPath = function(pack, path) {
 /**
  * Adds a sound to the pack library, it should pass an object with:
  *  pack_id: 	a unique identification for the pack
- *  label:		the label of this sound we're adding 
- *  file:		name of the sound file, NOT the full path 
+ *  label:		the label of this sound we're adding
+ *  file:		name of the sound file, NOT the full path
  *  icon:		the path to the icon file for this sound
  *
  */
 Packs.addSound = function(soundData) {
-	loadingStatus("Adding " + soundData.label + " to " + soundData.pack_id);  
-	
-	// Create a sounds total 
+	loadingStatus("Adding " + soundData.label + " to " + soundData.pack_id);
+
+	// Create a sounds total
 	this.soundsTotal++;
-	
+
 	// Generate the audio file ID (it's based off the mp3/wav file)
 	var id = soundData.file.substring(0, soundData.file.length - 4);
-	
+
 	this.audioObjects[id] = soundManager.createSound({
 		autoLoad: true,
 		url: this.soundsPaths[soundData.pack_id]  + soundData.file,
-		whileloading: function() { 
+		whileloading: function() {
 			Packs.audioReady();
 		}
 	});
-	
+
 	var iOS = /(iPad|iPhone|iPod)/g.test( navigator.userAgent );
 	if(iOS) {
-		// iOS cant autoload songs, so we just assume they're loaded 
+		// iOS cant autoload songs, so we just assume they're loaded
 		Packs.audioReady();
-	}	            
+	}
 	// We add it to a collection array, this allows us to quickly fetch all the sounds in a pack
 	if(this.collection[soundData.pack_id] == null) this.collection[soundData.pack_id] = [];
 	this.collection[soundData.pack_id].push(id);
-	
+
 	this.soundLabels[id] = soundData.label;
 };
 
@@ -112,13 +112,13 @@ Packs.setDefaultTrack = function(pack, data) {
  */
 Packs.getCollections = function() {
 	var results = [];
-	
+
 	for(var key in Packs.collection) {
 		if (Packs.collection.hasOwnProperty(key)) {
 			results.push(key);
 		}
 	}
-	
+
 	return results;
 };
 
@@ -130,10 +130,10 @@ Packs.loadPack = function(packURL) {
 	var s = document.createElement("script");
 	s.type = "text/javascript";
 	s.src = packURL;
-	
+
 	s.onreadystatechange = Packs.loadComplete;
 	s.onload = Packs.loadComplete;
-	
+
 	head.appendChild(s);
 };
 
@@ -141,7 +141,7 @@ Packs.getSound = function(id) {
 	return Packs.audioObjects["" + id];
 };
 
-// These variables are used to determine when we're finished loading 
+// These variables are used to determine when we're finished loading
 Packs.prepared = 0;
 Packs.loaded = 0;
 Packs.soundsTotal = 0;
@@ -149,16 +149,16 @@ Packs.soundsReady = 0;
 
 /**
  * Do not call. This is triggered when a script file has been loaded,
- * it is used to determine if all packs have loaded their script files. 
+ * it is used to determine if all packs have loaded their script files.
  */
 Packs.loadComplete = function() {
 	Packs.loaded++;
-	
+
 	loadingStatus(
 					Packs.loaded      + " / " + Packs.prepared    + " packs loaded. "  + "<br><br> " +
 					Packs.soundsReady + " / " + Packs.soundsTotal + " sounds ready."
-				 ); 
-	
+				 );
+
 	Packs.checkReady();
 };
 
@@ -168,70 +168,78 @@ Packs.loadComplete = function() {
 Packs.checkReady = function() {
 	if(Packs.soundsReady == Packs.soundsTotal && Packs.loaded == Packs.prepared) {
 		Mixer.loadExistingTracks();
-		
+
 		// This is attempting to fix issues with cross-browser stuff
 		runFixes();
-		
+
 		setTimeout(function() {
-			showStage("main");
 			Packs.buildList();
+
+			var url = new URL(window.location);
+			var load = url.searchParams.get("load");
+
+			if (load != null && load != "") {
+				Mixer.doImport(Base64.decode(load));
+			} else {
+				showStage("main");
+			}
 		});
 	}
 };
 
 /**
  * Do not call. This is triggered when a sound file has been loaded,
- * it is used to determine if all packs have loaded their sound files. 
+ * it is used to determine if all packs have loaded their sound files.
  */
 Packs.audioReady = function() {
 	loadingStatus(
 					Packs.loaded      + " / " + Packs.prepared    + " packs loaded. "  + "<br><br> " +
 					Packs.soundsReady + " / " + Packs.soundsTotal + " sounds ready."
-				 ); 
-	
+				 );
+
 	Packs.soundsReady++;
 	Packs.checkReady();
 };
 
 /**
- * Shows a pack, builds pack data. 
+ * Shows a pack, builds pack data.
  */
 Packs.show = function(pack) {
 	if(Packs.collection[pack] == null) return;
-	
+
 	var template = "<span id=\"%id%\" class=\"trackname\" draggable=\"true\" ondrag=\"window.dragID = this.id;\" ondragover=\"event.preventDefault();\"  onclick=\"Packs.preview('%id%');\" onmouseout=\"window.previewSound.pause();\">%name%</span>";
 	var master_template = "";
-	
-	Packs.collection[pack].forEach(function(id) { 
+
+	Packs.collection[pack].forEach(function(id) {
 		var add = template;
 		add = add.replaceAll("%id%", id);
 		add = add.replaceAll("%name%", Packs.soundLabels[id]);
-		
+
 		master_template+=add;
 	});
-	
+
 	document.getElementById("packs-data").innerHTML = master_template;
-	
+
 	if(runFixes != null) {
-		runFixes(); // run this again to fix dnd issues with certain browsers 
+		runFixes(); // run this again to fix dnd issues with certain browsers
 	}
-	
+
 };
 
 /**
- * Builds the collections list 
+ * Builds the collections list
  */
 Packs.buildList = function() {
 	var template = "<span class=\"packname\" onclick=\"Packs.show('%name%');\">%name%</span>";
 	var master_template = "";
-	
-	Packs.getCollections().forEach(function(name) { 
+
+	Packs.getCollections().forEach(function(name) {
 		var add = template;
 		add = add.replaceAll("%name%", name);
-		
+
 		master_template+=add;
 	});
-	
+
 	document.getElementById("packs-list").innerHTML = master_template;
 
 };
