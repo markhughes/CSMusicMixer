19ff02798f9f21b08ef6b494481166701680c4cf
Mark Hughes <markhughes@Marks-MacBook-Pro-2.local>
2025-12-30 20:27:08 +1100

    feat: github pages update

--- webpack.config.js
+++ webpack.config.js
@@ -4,49 +4,52 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const assets = path.resolve(__dirname, "assets");
 
-module.exports = {
-  entry: path.resolve(__dirname, "src/index.ts"),
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "auto",
-    clean: true,
-  },
-  devtool: "source-map",
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        { from: path.resolve(assets), to: path.resolve("dist", "assets") },
-      ],
-    }),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "index.html"),
-      filename: "index.html",
-      inject: "body",
-      scriptLoading: "blocking",
-    }),
-  ],
-  devServer: {
-    static: false,
-    port: 8080,
-    hot: false,
-    liveReload: true,
-    client: {
-      overlay: true,
-    },
-  },
-};
+module.exports = (env = {}) => {
+    const publicPath = env.PUBLIC_PATH ?? "auto";
 
+    return {
+        entry: path.resolve(__dirname, "src/index.ts"),
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: "bundle.js",
+            publicPath,
+            clean: true,
+        },
+        devtool: "source-map",
+        resolve: {
+            extensions: [".ts", ".js"],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: "ts-loader",
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        plugins: [
+            new CopyPlugin({
+                patterns: [
+                    { from: path.resolve(assets), to: path.resolve("dist", "assets") },
+                ],
+            }),
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, "index.html"),
+                filename: "index.html",
+                inject: "body",
+                scriptLoading: "blocking",
+            }),
+        ],
+        devServer: {
+            static: false,
+            port: 8080,
+            hot: false,
+            liveReload: true,
+            client: {
+                overlay: true,
+            },
+        },
+    };
+};
 
